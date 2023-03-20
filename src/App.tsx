521c8a9746fee81ce460a98bfb33901ffcc86b6b
Paul Aquino Gutierrez <paulaquino@sunedu.gob.pe>
2023-03-20 16:11:23 -0500

    refactor clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import './App.css'
-
 import Tree from './modules/tree/tree/tree'
 import { DATA } from './modules/tree/tree/tree.data'
 import { TreeEventData } from './modules/tree/tree/tree.data.interface'
+
 function App() {
   const data = DATA
   const data2 = structuredClone(DATA)
 
   const onChangeRecibedF = (treeEventData: TreeEventData<any>) => {
-
-    setTimeout(() => {
-      treeEventData.subject.setSubject({ event: treeEventData.treeData.eventConfirmation, data: treeEventData.treeData })
-    }, 0)
+    setTimeout(() => {treeEventData.subject.setSubject({ event: treeEventData.treeData.eventConfirmation, data: treeEventData.treeData })}, 0)
     console.log('RETORNO1', treeEventData)
   }
 
   const onChangeRecibedF2 = (treeEventData: TreeEventData<any>) => {
-    setTimeout(() => {
-      treeEventData.subject.setSubject({ event: treeEventData.treeData.eventConfirmation, data: treeEventData.treeData })
-    }, 0)
+    setTimeout(() => {treeEventData.subject.setSubject({ event: treeEventData.treeData.eventConfirmation, data: treeEventData.treeData })}, 0)
     console.log('RETORNO2', treeEventData)
   }
 
@@ -27,13 +22,9 @@ function App() {
       <Tree data={data} onChange={onChangeRecibedF} render={External} />
       <Tree data={data2} onChange={onChangeRecibedF2} />
     </>
-
   )
 }
+
 export default App
 
-export const External = (props: any) => {
-  return (
-    <div className="ux-external"> external {props.label + "HIJO DE P2"}</div>
-  );
-};
+export const External = (props: any) => {return (<div className="ux-external"> external {props.label + "HIJO DE P2"}</div>)}
