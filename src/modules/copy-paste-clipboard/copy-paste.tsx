f04c2d5618a359b8b3e4f416cd3b7dca58585cfc
Paul Aquino Gutierrez <paulaquino@sunedu.gob.pe>
2023-04-07 18:03:17 -0500

    clipboard all file types

--- src/modules/copy-paste-clipboard/copy-paste.tsx
+++ src/modules/copy-paste-clipboard/copy-paste.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './clipboard.css'
 import { v4 as uuidv4 } from 'uuid';
 
@@ -6,11 +6,10 @@ interface NodeElementClip {
   id: string
   type: string
   data: any
+  dataFile?:any
 }
 function ClipboardComponent() {
 
-  // const [text, setText] = useState('Pegue aqui');
-  // const [image, setImage] = useState('');
   const [nodeElmenteClip, setNodeElmenteClip] = useState<NodeElementClip[]>([{ id: uuidv4(), type: 'text', data: '...' }]);
 
   const ref = useRef(document.createElement("p"))
@@ -18,72 +17,57 @@ function ClipboardComponent() {
   const handlePaste = async (event: React.ClipboardEvent<HTMLDivElement>) => {
     event.preventDefault();
     const clipboardData = event.clipboardData;
-
+    console.log('clipboardData',clipboardData.files.length)
     if (clipboardData) {
-
       const textClip = clipboardData.getData('Text')
-
       if (textClip) {
-
         nodeElmenteClip.push({ id: uuidv4(), type: 'text', data: textClip })
         setNodeElmenteClip([...nodeElmenteClip])
-        //setText(textClip);
-
       }
 
       if (!textClip) {
         const items = await clipboardData.items;
         for (let i = 0; i < items.length; i++) {
           const item: any = items[i]
+          console.log('item',item.name)
+          const fileBlob = await item.getAsFile()
+          console.log('blop name', fileBlob.name,fileBlob )
           if (item.type.indexOf('image') !== -1) {
-            const blob = await item.getAsFile();
-            blob && nodeElmenteClip.push({ id: uuidv4(), type: 'image', data: URL.createObjectURL(blob) })
-            nodeElmenteClip.push({ id: uuidv4(), type: 'text', data: ' ' })
-            nodeElmenteClip.push({ id: uuidv4(), type: 'text', data: ' ' })
-            setNodeElmenteClip([...nodeElmenteClip]) //setImage(URL.createObjectURL(blob));
+            fileBlob && nodeElmenteClip.push({ id: uuidv4(), type: 'image', data: URL.createObjectURL(fileBlob),dataFile: fileBlob})
+          } else {
+            fileBlob && nodeElmenteClip.push({ id: uuidv4(), type: 'file', data: fileBlob.name,dataFile: fileBlob})
           }
         }
+        nodeElmenteClip.push({ id: uuidv4(), type: 'text', data: ' ' })
+        setNodeElmenteClip([...nodeElmenteClip])
       }
     }
 
-    //ref.current.focus()
-    //console.log( 'ref.current',  ref.current.children.length,ref)
-   
-   
-  //  let range = document.createRange()
-  //  let sel = window.getSelection()
-  //  console.log('current',ref.current.childNodes[ref.current.children.length-1])
-   
-  //  range.setStart(ref.current.childNodes[ref.current.children.length-1], 1)
-  //  range.collapse(true)
-   
-  //  sel?.removeAllRanges()
-  //  sel?.addRange(range)
-  
-  
   }
 
   useEffect(() => {
-   let range = document.createRange()
-   let sel = window.getSelection()
-   range.setStart(ref.current.childNodes[ref.current.children.length-1], 1)
-   range.collapse(true)
-   
-   sel?.removeAllRanges()
-   sel?.addRange(range)
+    let range = document.createRange()
+    let sel = window.getSelection()
+    range.setStart(ref.current.childNodes[ref.current.children.length - 1], 1)
+    range.collapse(true)
+
+    sel?.removeAllRanges()
+    sel?.addRange(range)
+    console.log('nodeElmenteClip',nodeElmenteClip)
     return () => {
-      
+
     }
   }, [nodeElmenteClip])
-  
+
   return (
-    <div className='su-clipboard' contentEditable={true} suppressContentEditableWarning={true}  onPaste={handlePaste} ref={ref}>
+    <div className='su-clipboard' contentEditable={true} suppressContentEditableWarning={true} onPaste={handlePaste} ref={ref}>
       {nodeElmenteClip.map((item, index) => {
         return (
-         <>
-            {item.type === 'text' &&<p id={item.id +index}>{item.data}</p>}
-            {item.type === 'image' && <img id={item.id} src={item.data}/>}
-         </>
+          <>
+            {item.type === 'text' && <p id={item.id + index}>{item.data}</p>}
+            {item.type === 'image' && <img id={item.id} src={item.data} />}
+            {item.type === 'file' && <p id={item.id}>{item.data}</p>}
+          </>
         )
       })}
     </div>
