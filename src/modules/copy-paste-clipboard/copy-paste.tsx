54fccb52f11e17e75f5d8b0ec54bae9aee2a27f0
Paul Aquino Gutierrez <paulaquino@sunedu.gob.pe>
2023-04-08 17:53:49 -0500

    change map for function component fot type

--- src/modules/copy-paste-clipboard/copy-paste.tsx
+++ src/modules/copy-paste-clipboard/copy-paste.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { ReactElement, useEffect, useRef, useState } from 'react';
 import './clipboard.css'
 import { v4 as uuidv4 } from 'uuid';
 
@@ -6,40 +6,47 @@ interface NodeElementClip {
   id: string
   type: string
   data: any
-  dataFile?:any
+  dataFile?: any
 }
 function ClipboardComponent() {
 
-  const [nodeElmenteClip, setNodeElmenteClip] = useState<NodeElementClip[]>([{ id: uuidv4(), type: 'text', data: '...' }]);
+  const [nodeElmenteClip, setNodeElmenteClip] = useState<NodeElementClip[]>([{ id: uuidv4(), type: 'text', data: ' ' }]);
+  const [componentList, setComponentList] = useState<any[]>([TextComponent(uuidv4(), '...')]);
 
   const ref = useRef(document.createElement("p"))
 
   const handlePaste = async (event: React.ClipboardEvent<HTMLDivElement>) => {
     event.preventDefault();
     const clipboardData = event.clipboardData;
-    console.log('clipboardData',clipboardData.files.length)
+    console.log('clipboardData', clipboardData.files.length)
     if (clipboardData) {
       const textClip = clipboardData.getData('Text')
       if (textClip) {
         nodeElmenteClip.push({ id: uuidv4(), type: 'text', data: textClip })
         setNodeElmenteClip([...nodeElmenteClip])
+        componentList.push(TextComponent(uuidv4(), textClip))
+        setComponentList([...componentList])
       }
 
       if (!textClip) {
         const items = await clipboardData.items;
         for (let i = 0; i < items.length; i++) {
           const item: any = items[i]
-          console.log('item',item.name)
+          console.log('item', item.name)
           const fileBlob = await item.getAsFile()
-          console.log('blop name', fileBlob.name,fileBlob )
+          console.log('blop name', fileBlob.name, fileBlob)
           if (item.type.indexOf('image') !== -1) {
-            fileBlob && nodeElmenteClip.push({ id: uuidv4(), type: 'image', data: URL.createObjectURL(fileBlob),dataFile: fileBlob})
+            fileBlob && nodeElmenteClip.push({ id: uuidv4(), type: 'image', data: URL.createObjectURL(fileBlob), dataFile: fileBlob })
+            fileBlob && componentList.push(ImgeComponent(uuidv4(), URL.createObjectURL(fileBlob)))
           } else {
-            fileBlob && nodeElmenteClip.push({ id: uuidv4(), type: 'file', data: fileBlob.name,dataFile: fileBlob})
+            fileBlob && nodeElmenteClip.push({ id: uuidv4(), type: 'file', data: fileBlob.name, dataFile: fileBlob })
+            fileBlob && componentList.push(FileComponent(uuidv4(), fileBlob.name))
           }
         }
         nodeElmenteClip.push({ id: uuidv4(), type: 'text', data: ' ' })
         setNodeElmenteClip([...nodeElmenteClip])
+        componentList.push(TextComponent(uuidv4(), ' '))
+        setComponentList([...componentList])
       }
     }
 
@@ -48,59 +55,76 @@ function ClipboardComponent() {
   useEffect(() => {
     let range = document.createRange()
     let sel = window.getSelection()
-    range.setStart(ref.current.childNodes[ref.current.children.length - 1], 1)
+    const ultimoNodo: any = ref.current.childNodes[ref.current.children.length - 1]
+    //const longitudUltimoNodo:number =ultimoNodo.childNodes[1].length
+    range.setStart(ultimoNodo, 2)
     range.collapse(true)
-
     sel?.removeAllRanges()
     sel?.addRange(range)
-    console.log('nodeElmenteClip',nodeElmenteClip)
+
     return () => {
 
     }
-  }, [nodeElmenteClip])
+  }, [componentList])
 
 
   // drag and drop section :
   const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
-    console.log('1 handleDragEnter entra area',e);
+    console.log('1 handleDragEnter entra area', e);
   };
 
   const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
-    console.log('3 handleDragLeave sale area',e);
+    console.log('3 handleDragLeave sale area', e);
   };
 
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
-   // console.log('2 handleDragOver se mantiene en el area',e);
+    // console.log('2 handleDragOver se mantiene en el area',e);
   };
 
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
 
-    console.log('handleDrop',e.dataTransfer.files);
+    console.log('handleDrop', e.dataTransfer.files);
 
-    const items =  e.dataTransfer.files;
+    const items = e.dataTransfer.files;
     for (let i = 0; i < items.length; i++) {
       const item: any = items[i]
-      console.log('item',item)
+      console.log('item', item)
       const fileBlob = item
- 
+
       if (item.type.indexOf('image') !== -1) {
-        fileBlob && nodeElmenteClip.push({ id: uuidv4(), type: 'image', data: URL.createObjectURL(fileBlob),dataFile: fileBlob})
+        fileBlob && nodeElmenteClip.push({ id: uuidv4(), type: 'image', data: URL.createObjectURL(fileBlob), dataFile: fileBlob })
+        fileBlob && componentList.push(ImgeComponent(uuidv4(), URL.createObjectURL(fileBlob)))
       } else {
-        fileBlob && nodeElmenteClip.push({ id: uuidv4(), type: 'file', data: fileBlob.name,dataFile: fileBlob})
+        fileBlob && nodeElmenteClip.push({ id: uuidv4(), type: 'file', data: fileBlob.name, dataFile: fileBlob })
+        fileBlob && componentList.push(FileComponent(uuidv4(), fileBlob.name))
       }
     }
     nodeElmenteClip.push({ id: uuidv4(), type: 'text', data: ' ' })
     setNodeElmenteClip([...nodeElmenteClip])
+    componentList.push(TextComponent(uuidv4(), ' '))
+    setComponentList([...componentList])
 
   };
+
+  function TextComponent(id: string, data: string) {
+    return (<p key={id}> {data}</p>)
+  }
+
+  function ImgeComponent(id: string, data: string) {
+    return (<img key={id} src={data} />)
+  }
+
+  function FileComponent(id: string, data: string) {
+    return (<p key={id}> {data}</p>)
+  }
 
   return (
     <div className='su-clipboard' contentEditable={true} suppressContentEditableWarning={true} onPaste={handlePaste} ref={ref}
@@ -109,15 +133,7 @@ function ClipboardComponent() {
       onDragOver={handleDragOver}
       onDrop={handleDrop}
     >
-      {nodeElmenteClip.map((item, index) => {
-        return (
-          <>
-            {item.type === 'text' && <p id={item.id + index}>{item.data}</p>}
-            {item.type === 'image' && <img id={item.id} src={item.data} />}
-            {item.type === 'file' && <p id={item.id}>{item.data}</p>}
-          </>
-        )
-      })}
+      {componentList.map(item => item)}
     </div>
   )
 
